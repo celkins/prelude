e9228ef2a95d7e23bf8d069b03da6779e1eaf6fc
Christopher Elkins <celkins@users.noreply.github.com>
2015-05-28 10:42:45 -0700

    Use GNU ls when available on OS X

---
+++ personal/personal-darwin.el
@@ -0,0 +1,15 @@
+;;; personal-darwin --- Personal OS X configuration
+
+;;; Commentary:
+;;
+;; This loads my personal configuration for OS X.
+
+;;; Code:
+
+(when (eq system-type 'darwin
+
+        ;; n.b., `brew install coreutils'
+        (setq insert-directory-program (executable-find "gls"))))
+
+(provide 'personal-darwin)
+;;; personal-darwin ends here
