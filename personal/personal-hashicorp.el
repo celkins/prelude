67421f0247e511e03ea24bef866824767068c2e9
Christopher Elkins <celkins@users.noreply.github.com>
2016-06-01 18:20:04 -0700

    Add support for Terraform et al

---
+++ personal/personal-hashicorp.el
@@ -0,0 +1,7 @@
+;;; personal-hashicorp --- Personal configuration for Hashicorp-related packages
+
+;;; Commentary:
+;;
+;; This loads my personal configuration for Hashicorp-related packages
+
+(prelude-require-packages '(hcl-mode terraform-mode))
