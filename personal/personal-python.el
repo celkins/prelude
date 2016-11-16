3fa268921f40ee4f1943f9acd524b30aac6cd49f
Christopher Elkins <celkins@users.noreply.github.com>
2016-11-16 08:28:31 -0800

    Activate pyenv-mode only when necessary

--- personal/personal-python.el
+++ personal/personal-python.el
@@ -1,10 +1,10 @@
-(prelude-require-packages '(pyenv-mode))
-
-(pyenv-mode)
+(prelude-require-packages '(pyenv-mode pyenv-mode-auto))
 
 (defun projectile-pyenv-mode-set ()
-  "Set pyenv version matching project name.
-Version must be already installed."
-  (pyenv-mode-set (projectile-project-name)))
+  "Set pyenv version matching project name."
+  (let ((project (projectile-project-name)))
+    (if (member project (pyenv-mode-versions))
+        (pyenv-mode-set project)
+      (pyenv-mode-unset))))
 
 (add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set)
