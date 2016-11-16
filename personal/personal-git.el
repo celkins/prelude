2b1bcf17e30514002b3b313b0696f5928871b84e
Christopher Elkins <celkins@users.noreply.github.com>
2016-11-16 08:42:04 -0800

    Load git-commit-mode globally

---
+++ personal/personal-git.el
@@ -0,0 +1,7 @@
+;;; personal-sml --- Personal configuration for Git-related packages
+
+;;; Commentary:
+;;
+;; This loads my personal configuration for Git-related packages
+
+(global-git-commit-mode)
