1ab1932080b934db850f0b3f7d040de61744a959
Christopher Elkins <celkins@users.noreply.github.com>
2016-11-16 08:29:24 -0800

    Add basic SML config

---
+++ personal/personal-sml.el
@@ -0,0 +1,7 @@
+;;; personal-sml --- Personal configuration for SML-related packages
+
+;;; Commentary:
+;;
+;; This loads my personal configuration for SML-related packages
+
+(prelude-require-packages '(sml-mode))
